1a893199fde4ac682c026dc8b50aa31b8369e874
Franck Freiburger <franck.freiburger@gmail.com>
2018-12-02 10:22:09 +0100

    - add NoSssNoWorker mode

---
+++ src/vuePdfNoSssNoWorker.vue
@@ -0,0 +1,17 @@
+<style src="./annotationLayer.css"></style>
+<script>
+
+	import componentFactory from './componentFactory.js'
+
+	if ( process.env.VUE_ENV !== 'server' ) {
+
+		var pdfjsWrapper = require('./pdfjsWrapper.js').default;
+		var PDFJS = require('pdfjs-dist/build/pdf.js');
+		var component = componentFactory(pdfjsWrapper(PDFJS));
+	} else {
+
+		var component = componentFactory({});
+	}
+
+	export default component;
+</script>
