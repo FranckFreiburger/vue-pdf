b7a606c30d7abfb548bd02bf3872795ce8fd87da
Lisandro <lisandrop05@gmail.com>
2019-12-12 17:47:03 -0500

    change pdfjs-dist library to include digital signature showing on document

--- src/pdfjsWrapper.js
+++ src/pdfjsWrapper.js
@@ -189,7 +189,8 @@ export default function(PDFJS) {
 				return;
 
 			rotate = (pdfPage.rotate === undefined ? 0 : pdfPage.rotate) + (rotate === undefined ? 0 : rotate);
-			canvasElt.getContext('2d').restore();
+			// canvasElt.getContext('2d').restore();
+			clearCanvas();
 			var scale = canvasElt.offsetWidth / pdfPage.getViewport(1).width * (window.devicePixelRatio || 1);
 			var viewport = pdfPage.getViewport(scale, rotate);
 
